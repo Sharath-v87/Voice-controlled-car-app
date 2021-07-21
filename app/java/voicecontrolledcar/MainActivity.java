94eaf5448f93ef15c5934a201d18d979399a18b0
Shawrat <sharathvenkateswaran@gmail.com>
2021-07-21 22:10:29 +0530

    half assed version

--- app/java/voicecontrolledcar/MainActivity.java
+++ app/java/voicecontrolledcar/MainActivity.java
@@ -5,11 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.provider.Settings;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
@@ -22,14 +28,26 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Objects;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textview_first;
     private Button audio_button;
     private static final int REQUEST_CODE_SPEECH_INPUT = 1;
+    public final static String MODULE_MAC = "FC:A8:9A:00:63:33";
+    public final static int REQUEST_ENABLE_BT = 1;
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+    BluetoothAdapter bta;                 //bluetooth stuff
+    BluetoothSocket mmSocket;             //bluetooth stuff
+    BluetoothDevice mmDevice;             //bluetooth stuff
+    ConnectedThread btt = null;           //Our custom thread
+    public Handler mHandler;              //this receives messages from thread
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,7 +75,20 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        bta = BluetoothAdapter.getDefaultAdapter();
+        //if bluetooth is not enabled then create Intent for user to turn it on
+        if(!bta.isEnabled()){
+            Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBTIntent, REQUEST_ENABLE_BT);
+        }
+        else{
+            initiateBluetoothProcess();
+        }
     }
+
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -65,8 +96,49 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK && data != null) {
                 ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                 textview_first.setText(Objects.requireNonNull(result).get(0));
+                initiateBluetoothProcess();
+
             }
         }
     }
+
+    public void initiateBluetoothProcess(){
+        if(bta.isEnabled()){
+            //attempt to connect to bluetooth module
+            BluetoothSocket tmp = null;
+            mmDevice = bta.getRemoteDevice(MODULE_MAC);
+            //create socket
+            try {
+                tmp = mmDevice.createRfcommSocketToServiceRecord(MY_UUID);
+                mmSocket = tmp;
+                mmSocket.connect();
+                Log.i("[BLUETOOTH]","Connected to: "+mmDevice.getName());
+            }
+            catch(IOException e){
+                try{mmSocket.close();}catch(IOException c){return;}
+            }
+            Log.i("[BLUETOOTH]", "Creating handler");
+            mHandler = new Handler(Looper.getMainLooper()){
+                @Override
+                public void handleMessage(Message msg) {
+                    super.handleMessage(msg);
+                    if(msg.what == ConnectedThread.RESPONSE_MESSAGE){
+                        String txt = (String)msg.obj;
+                        if(textview_first.getText().toString().length() >= 30){
+                            textview_first.setText("");
+                            textview_first.append(txt);
+                        }else{
+                            textview_first.append("\n" + txt);
+                        }
+                    }
+                }
+            };
+            Log.i("[BLUETOOTH]", "Creating and running Thread");
+            btt = new ConnectedThread(mmSocket,mHandler);
+            btt.start();
+        }
+    }
+
+
 
 }
